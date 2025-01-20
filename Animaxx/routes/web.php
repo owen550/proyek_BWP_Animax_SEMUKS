cc364aa343576c4c4dd57ec3d3bdec50ec21e985
KevinAK17 <kevinak1717@gmail.com>
2025-01-20 23:21:49 +0700

    upload album

--- Animaxx/routes/web.php
+++ Animaxx/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\loginRegisterControler;
 use App\Http\Controllers\MainMenuControler;
 use App\Http\Controllers\ProfileControler;
+use App\Http\Controllers\AlbumController;
 use Illuminate\Support\Facades\Route;
 
 // untuk model
@@ -55,3 +56,7 @@ Route::get('/album/watch',function(){
 });
 
 
+
+Route::get('/uploadAlbum', [AlbumController::class, 'create'])->name('upload.album');
+
+Route::post('/uploadAlbum', [AlbumController::class, 'store'])->name('uploadAlbum.store');
